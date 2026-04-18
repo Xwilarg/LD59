8dd4736f91d29b70228221be89dc6107dfe39491
Xwilarg <xwilarg@protonmail.com>
2026-04-18 11:15:22 +0200

    Add train spawning

--- Assets/Scripts/Map/Wagon.cs
+++ Assets/Scripts/Map/Wagon.cs
@@ -4,7 +4,16 @@ namespace LD59.Map
 {
     public class Wagon : MonoBehaviour
     {
+        [SerializeField]
+        public Transform _forward, _backward;
+
         public Vector2 TilePos { set; get; }
         public Exit Direction { set; get; }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(_forward.position, _backward.position);
+        }
     }
 }
