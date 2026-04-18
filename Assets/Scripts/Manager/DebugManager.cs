38fc7d71a799f3d24d7f1c61194171d9cb0b34a2
Xwilarg <xwilarg@protonmail.com>
2026-04-18 10:51:37 +0200

    Add debug menu

---
+++ Assets/Scripts/Manager/DebugManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace LD59.Manager
+{
+    public class DebugManager : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject _debugMenu;
+
+        private void Awake()
+        {
+#if !UNITY_EDITOR
+            _debugMenu.SetActive(false);
+#endif
+        }
+
+        public void SpawnTrain()
+        {
+
+        }
+    }
+}
