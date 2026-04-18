99616a8feedfda5f0427f90f6947d3364eea34b8
Xwilarg <xwilarg@protonmail.com>
2026-04-18 10:44:39 +0200

    Improve platform management

--- Assets/Scripts/Manager/MapManager.cs
+++ Assets/Scripts/Manager/MapManager.cs
@@ -1,4 +1,5 @@
 ﻿using LD59.Map;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace LD59.Manager
@@ -8,15 +9,30 @@ namespace LD59.Manager
         [SerializeField]
         private GameObject _railPrefab;
 
+        private readonly List<Platform> _platforms = new();
+
         private void Start()
         {
-            var go = Instantiate(_railPrefab, new Vector2(0, -7) * (GridManager.GridSize / 100f), Quaternion.identity);
+            PlacePlatform(new(0, -7), Exit.Up);
+        }
+
+        private void PlacePlatform(Vector2Int pos, Exit usedExit)
+        {
+            var go = Instantiate(_railPrefab, (Vector2)pos * (GridManager.GridSize / 100f), Quaternion.identity);
             var rail = go.GetComponent<Rail>();
             var platform = SpriteManager.Instance.GetPlatform();
             rail.SR.sprite = platform.Sprite;
             rail.Exits = platform.Exits;
             rail.CanOverrides = false;
-            GridManager.Instance.Register(new(0, -7), rail);
+            GridManager.Instance.Register(pos, rail);
+
+            _platforms.Add(new() { Position = pos, Exit = usedExit });
         }
+    }
+
+    public class Platform
+    {
+        public Vector2Int Position;
+        public Exit Exit;
     }
 }
