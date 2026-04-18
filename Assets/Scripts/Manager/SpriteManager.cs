206832629b8ed31759d62dbc41c71c16ced0c95e
Xwilarg <xwilarg@protonmail.com>
2026-04-18 09:12:23 +0200

    Add T crossing

--- Assets/Scripts/Manager/SpriteManager.cs
+++ Assets/Scripts/Manager/SpriteManager.cs
@@ -7,7 +7,7 @@ namespace LD59.Manager
         public static SpriteManager Instance { private set; get; }
 
         [SerializeField]
-        private Sprite _straight, _turn, _crossing;
+        private Sprite _straight, _turn, _crossing, _tCrossing;
 
         private void Awake()
         {
@@ -16,11 +16,17 @@ namespace LD59.Manager
 
         public RotatedSprite GetHorizontal() => new() { Sprite = _straight, Rotation = 90f };
         public RotatedSprite GetVertical() => new() { Sprite = _straight, Rotation = 0f };
+
         public RotatedSprite GetTurnDownRight() => new() { Sprite = _turn, Rotation = 0f };
         public RotatedSprite GetTurnUpRight() => new() { Sprite = _turn, Rotation = 90f };
         public RotatedSprite GetTurnUpLeft() => new() { Sprite = _turn, Rotation = 180f };
         public RotatedSprite GetTurnDownLeft() => new() { Sprite = _turn, Rotation = 270f };
-        public RotatedSprite GetCrossing() => new() { Sprite = _crossing, Rotation = 0f };
+
+        public RotatedSprite GetCrossingAll() => new() { Sprite = _crossing, Rotation = 0f };
+        public RotatedSprite GetCrossingTDownLeftRight() => new() { Sprite = _tCrossing, Rotation = 0f };
+        public RotatedSprite GetCrossingTDownUpRight() => new() { Sprite = _tCrossing, Rotation = 90f };
+        public RotatedSprite GetCrossingTUpLeftRight() => new() { Sprite = _tCrossing, Rotation = 180f };
+        public RotatedSprite GetCrossingTDownUpLeft() => new() { Sprite = _tCrossing, Rotation = 270f };
     }
 
     public record RotatedSprite
